d05c9cebb9078606f3f928db42cffdef2185b46c
matt_hou <zombie666@gmail.com>
2014-05-26 20:14:07 +0800

    render and partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
 
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def udpate
+
+  end
+
+
 end
