043d3c6e0b78b7860725b523feca1758f027f5ee
matt_hou <zombie666@gmail.com>
2014-05-27 10:13:05 +0800

    create new post feature and validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,29 @@
 class PostsController < ApplicationController
   def index
     @post = Post.all
-
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def create
-
-  end
-
   def new
-
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
+
+    if @post.save
+      flash[:notice] = "Your post was created"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+
 
   def edit
 
@@ -24,5 +33,10 @@ class PostsController < ApplicationController
 
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
 
 end
