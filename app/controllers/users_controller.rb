8820c58151c84fd30ec554dc0d50ca7e0f1a5357
matt_hou <zombie666@gmail.com>
2014-06-08 22:29:53 +0800

    edit/show user debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -40,6 +41,13 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do this."
+      redirect_to root_path
+    end
   end
 
 end
